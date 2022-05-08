2b091e868d9803bddbf53866e6f056d0ed76482e
Stepan <97447563+Stepanda1@users.noreply.github.com>
2022-05-08 20:22:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 'use strict'
-let min = Number(prompt('Введите от какого числа:'));
-let max = Number(prompt('Введите до какого числа:'));
-let n = Number(prompt('Введите кол-во значений:'));
+let min = 0;
+let max = 0;
+let n = 0;
+min = Number(prompt('Введите от какого числа:'));
+max = Number(prompt('Введите до какого числа:'));
+n = Number(prompt('Введите кол-во значений:'));
 
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n; i) {
 
     let numRandom = min + (Math.random() * max - min);
     let numRandomRound = Math.round(numRandom);
